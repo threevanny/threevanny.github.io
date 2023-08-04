22265cac5f1f943b8cdde6f4619cbf75d01788df
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2023-08-04 15:20:47 -0500

    Basic layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <header className="bg-gray-900 text-yellow-300 p-4">
+            <div className="container mx-auto flex items-center justify-between">
+                <div className="text-lg font-bold">Threevanny</div>
+            </div>
+        </header>
+    )
+}
